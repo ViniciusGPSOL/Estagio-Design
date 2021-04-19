d993cb5aab1e42cbc267e2f0e5033549882f8995
ViniciusGPSOL <80440262+ViniciusGPSOL@users.noreply.github.com>
2021-04-19 12:04:20 -0300

    Class de parametro para dados e lista para dados de gráfico
    Conversão dos dados da lista de string para integer e organização dos mesmos em uma lista.

--- ler_info.py
+++ ler_info.py
@@ -1,11 +1,19 @@
-#ler informações gerais
-
+#Abrir arquivo.txt e separar cada linha em uma lista. 
 arq = open('info_g.txt', 'r')
-
 info = (arq.readlines())
 
-print(info[2], info[3])
+#Printar informacoes
+print(info[0], info[1], info[2])
 
-'''for linha in info:
-    print(linha)
-'''
+#classe para colocar dados nos parâmetros necessários
+lista_dados = []
+class dados_grafico(object):
+	def __init__(self, dado):
+		self.dado = int((float(dado[5:])))
+
+for element in info:
+	if element[4:5] == '#':
+		dado_convertido = dados_grafico(element)
+		lista_dados.append(dado_convertido.dado)
+
+print(lista_dados)
