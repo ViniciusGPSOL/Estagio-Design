29d8f35faaccfa62816f330dde4ffe478142aadf
ViniciusGPSOL <80440262+ViniciusGPSOL@users.noreply.github.com>
2021-04-21 09:21:41 -0300

    Grafico de barras e pizza com leitura de dados

--- Gráficos/Exemplo gráfico Pyqt.py
+++ Gráficos/Exemplo gráfico Pyqt.py
@@ -1,18 +1,13 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import ler_info
+import ler_info as li
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import QApplication, QWidget
 
 #Abrir arquivo.txt e separar cada linha em uma lista. 
 arq = open('info_g.txt', 'r')
 info = (arq.readlines())
-
-#classe para colocar dados nos parâmetros necessários
-class dados_grafico(object):
-    def __init__(self, dado):
-        self.dado = int((float(dado[5:])))
 
 #define a tela com as escalas e informações do gráfico
 class Canvas(FigureCanvas):
@@ -24,13 +19,13 @@ class Canvas(FigureCanvas):
         """ 
         Dados do gráfico
         """
-        xpoints = np.array(colunas)
-        ypoints = np.array(lista_dados)
+        xpoints = np.array(li.colunas)
+        ypoints = np.array(li.lista_dados_num)
         
         self.ax.bar(xpoints, ypoints)
 
-        self.ax.set(xlabel=info[2][4:], ylabel=info[3][4:],
-            title=info[1][4:])
+        self.ax.set(xlabel=li.x, ylabel=li.y,
+            title=li.titulo)
         """self.ax.grid()"""
 
 #Define a resolução
@@ -42,20 +37,16 @@ class AppGrafico(QWidget):
         chart = Canvas(self)
 
 #listas com os dados organizados para o gráfico
-lista_dados = []
-colunas = []
+#lista_dados_num = []
+#colunas = []
 
-#Converter e adicionar os dados para as listas de dados do gráfico 
+#adicionar os dados para as listas de dados do gráfico 
 for element in info:
     if element[4:5] == '#':
-        dado_convertido = dados_grafico(element)
-        lista_dados.append(dado_convertido.dado)
-        coluna = lista_dados.index(dado_convertido.dado) + 1
-        colunas.append(info[2][4:] + ' ' + str(coluna))
+        element_unico = li.dados_grafico(element, info[2])
+        element_unico.converter_dados_num()
 
 app = QApplication(sys.argv)   
 janela = AppGrafico()
 janela.show()
 sys.exit(app.exec_())
-
-
