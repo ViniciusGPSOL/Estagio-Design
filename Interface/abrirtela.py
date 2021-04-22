f1b2b753f89cbcff3248274bf44897840d4e3b57
RenanBpoy <80440696+RenanBpoy@users.noreply.github.com>
2021-04-22 10:15:10 -0300

    Abrirtela atualizado

--- Interface/abrirtela.py
+++ Interface/abrirtela.py
@@ -8,7 +8,7 @@ class Janela(QtWidgets.QMainWindow):
      def __init__(self):
           super(Janela, self).__init__()
           uic.loadUi('ui/janela.ui', self)
-          self.setFixedSize(777, 712)   
+          self.setFixedSize(777, 712)
 
 class TelaInicial(QtWidgets.QMainWindow):
      def __init__(self):
@@ -16,13 +16,10 @@ class TelaInicial(QtWidgets.QMainWindow):
           uic.loadUi('ui/tela_principal.ui', self)
           self.setFixedSize(777, 712)
           self.setWindowTitle('Tela principal')
-          
-          self.pushButton.clicked.connect(self.on_pushButton_clicked)
           self.janela = Janela()
-
-     def on_pushButton_clicked(self):
-          self.open_dialog_box()
-          #self.janela.show()
+          
+          self.pushButton.clicked.connect(self.open_dialog_box)
+          
 
      def open_dialog_box(self):
           filepath = QFileDialog.getOpenFileName()
@@ -31,7 +28,8 @@ class TelaInicial(QtWidgets.QMainWindow):
 
           with open(filename, "r") as f:
                print(f.readline())
-
+               
+          self.janela.show()
           self.pushButton.clicked.disconnect(self.open_dialog_box)
 
 
