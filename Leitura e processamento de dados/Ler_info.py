e5f8b3a7d3f9ac7174a49cb363fa6564dd6800fb
ViniciusGPSOL <80440262+ViniciusGPSOL@users.noreply.github.com>
2021-05-04 01:32:51 -0300

    Projeto 2.0

---
+++ Leitura e processamento de dados/Ler_info.py
@@ -0,0 +1,70 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import logo_rc
+        
+class TelaInicial(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(TelaInicial, self).__init__()
+        uic.loadUi('ui/tela_principal.ui', self)
+        self.setFixedSize(777, 712)
+        self.setWindowTitle('Tela principal')
+        self.pushButton.clicked.connect(self.open_dialog_box)
+          
+
+    def open_dialog_box(self):
+        filepath = QFileDialog.getOpenFileName()
+        filename = filepath[0]
+        with open(filename, "r") as f:
+            global info
+            info = (f.readlines())
+#iformações primeira linha
+            div_linha1 = info[0].split(";")
+            titulo_grafico = div_linha1[0][3:]
+            tipo_grafico = div_linha1[1]
+            cor_fundo = div_linha1[2]
+#informações segunda linha
+            div_linha2 = info[1].split(";")
+            parametros = []
+            for param in div_linha2:
+                if param[:3] == '002':
+                    par1 = param[3:]
+                    parametros.append(par1)
+                else:
+                    parx = param
+                    parametros.append(parx)
+#informações terceira linha
+            div_linha3 = info[2].split(";")
+            titulo_coluna = div_linha3[0][3:]
+#informações quarta linha
+            div_linha4 = info[3].split(";")
+            titulo_linha = div_linha4[0][3:]
+#informação quinta linha
+            div_linha5 = info[4].split("|")
+            lista_valores = []
+            t= 0
+            while t != len(div_linha5):
+                lista_elementos_str = div_linha5[t].split(';')
+                lista_elementos =[]
+                for val in lista_elementos_str:
+                    if val[:3] == '005':
+                        first = val[3:]
+                        lista_elementos.append(int(first))
+                    else:
+                        lista_elementos.append(int(val))
+                t += 1
+                lista_valores.append(lista_elementos)
+            self.pushButton.clicked.disconnect(self.open_dialog_box)
+
+
+if __name__ == '__main__':
+     import sys
+     app = QtWidgets.QApplication(sys.argv) 
+     window = TelaInicial()
+     window.show()
+     sys.exit(app.exec_())
